d0c3d5c95811d23076edcc5312d48ac8bd4e3825
Patrick Vilchez <pvilchez794@gmail.com>
2022-06-23 09:45:11 -0500

    Poetry add as dependency manager

--- app.py
+++ app.py
@@ -1,33 +1,37 @@
-import pandas as pd
-import json
-import os
+from typing import List
 from dotenv import load_dotenv
-import requests
+import pandas as pd, json, os, requests
+
 load_dotenv()
-plates_data = pd.read_csv("ORBITEC_SIMPLIROUTE.csv")
+
+TOKEN = os.getenv('TOKEN')
+URL = os.getenv('SIMPLIROUTE_URL')
+PLATES_DATA = pd.read_csv("ORBITEC_SIMPLIROUTE.csv")
 
 arr = []
-for plate in plates_data.Dispositivo:
+
+for plate in PLATES_DATA.Dispositivo:
     arr.append(plate)
 
 print(arr)
 
-URL = os.getenv('SIMPLIROUTE_URL')
 headers = {
   'Content-Type': 'application/json',
-  'Authorization': 'Token ' + os.getenv('TOKEN')
+  'Authorization': f'Token {TOKEN}'
 }
 
-response = None
+# response = None
+
 try:
   response = requests.get(URL, headers=headers)
-  
+
   if response.status_code == 200:
     print(response.json())
     print("Success")
   else:
     print("Error")
     print(response.json())
+
 except Exception as e:
   print(e)
   print("Error")
